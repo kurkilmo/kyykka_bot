65fcb63b04f9ac4fe28e448a9f4d133a30abf938
kurkilmo <ilmo.kurki00@gmail.com>
2025-09-02 15:21:30 +0300

    Remove path from .env config

--- kyykka_bot/config.py
+++ kyykka_bot/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Ladataan .env-tiedosto
-load_dotenv('/Users/nikoiljin/tgBot/kyykkäprojekti/.env')
+load_dotenv('.env')
 
 # Telegram Botin Token ja Chat ID
 TG_TOKEN = os.getenv("TG_TOKEN")
