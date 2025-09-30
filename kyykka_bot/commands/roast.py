723c6bc6ad87d4a83af81b4c36e6b9a544bcd7ef
kurkilmo <ilmo.kurki00@gmail.com>
2025-09-30 15:20:20 +0300

    Add chat id logging to roasts

--- kyykka_bot/commands/roast.py
+++ kyykka_bot/commands/roast.py
@@ -6,6 +6,7 @@ from config import ROASTS
 
 async def roast(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """ Lähettää satunnaisen roastin """
+    print(f"Roast from {update.effective_chat.id}")
     text = update.message.text
     target = re.search(r'@[\w_]+', text)
     target_name = target.group(0) if target else "Tähtipelaaja"
